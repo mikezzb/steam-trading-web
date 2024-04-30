42b4e88eee55b276d9ca3602cdd462cb70dcad55
mikezzb <mikezhoudev@gmail.com>
2024-04-30 00:58:23 -0400

    feat: item page skeleton

---
+++ src/app/items/[itemId]/page.tsx
@@ -0,0 +1,61 @@
+import { ApiRoutes, getItem } from "@/apis";
+import { fetchServerData } from "@/apis/server";
+import { ApiConfig, AppConfig } from "@/config";
+import { getTotalPages } from "@/utils";
+import { Metadata, ResolvingMetadata } from "next";
+import { FC } from "react";
+import ItemPage from "./item-page";
+
+type Props = {
+  params: {
+    itemId: string;
+  };
+};
+
+const Page: FC<Props> = (props) => <ItemPage params={props.params} />;
+
+export const fetchAllItems = async () => {
+  const items: Item[] = [];
+  let totalPages = 1;
+  let page = 1;
+  while (page <= totalPages) {
+    // format query string
+    const query = new URLSearchParams({ page: page.toString() });
+    const endpoint = `items?${query}`;
+    const { items: pageItems, total } = await fetchServerData<ItemsData>(
+      endpoint
+    );
+    items.push(...pageItems);
+    totalPages = getTotalPages(total, ApiConfig.itemPageSize);
+    page++;
+  }
+  return items;
+};
+
+export async function generateStaticParams() {
+  const items = await fetchAllItems();
+  console.log(`Fetched ${items.length} items`);
+
+  return items.map((item) => ({
+    itemId: item._id,
+  }));
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const itemId = params.itemId;
+  const endpoint = `${ApiRoutes.items}/${itemId}`;
+  const { item } = await fetchServerData<{ item: Item }>(endpoint);
+
+  return {
+    title: `${item.name} - ${AppConfig.name}`,
+    description: `${item.name}, ${AppConfig.description}`,
+  };
+}
+
+export const dynamic = "force-static";
+export const dynamicParams = false;
+
+export default Page;
